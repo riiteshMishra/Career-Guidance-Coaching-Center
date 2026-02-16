1d15922f62af1311ee324ecb2303e374ea7cf96c
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-02-16 08:36:29 +0530

    push commit

--- src/components/core/home/Leftside.jsx
+++ src/components/core/home/Leftside.jsx
@@ -12,11 +12,23 @@ const Leftside = () => {
       <h1 className="text-lg font-bold leading-tight  text-(--primary-color) dynamic-medium-text">
         Complete English Grammar & Full Book Coverage
       </h1>
+
+      <p className="mt-4 text-gray-300  leading-tite max-w-lg">
+        At our institute, we ensure 100% completion of the
+        <span className="text-yellow-400 font-semibold text-justify text-[clamp(1rem,2vw,1.6rem)]">
+          {" "}
+          Class 9th and 10th English syllabus{" "}
+        </span>
+        with detailed explanations, regular practice tests, and exam-focused
+        preparation.
+      </p>
+
+      {/* 
       <p className="mt-4 text-gray-100 dynamic-small-text max-w-xl text-justify">
         Throughout the academic year, we guarantee complete English grammar
         mastery along with detailed explanation of every chapter from the
         prescribed textbook.
-      </p>
+      </p> */}
 
       <p className="mt-4 text-lg text-gray-100">
         At our institute, even academically weak students receive individual
